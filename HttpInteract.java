23376ff29e65a403098c76b6e96d0837c53ea2ed
Daniel <daniel@Daniels-MacBook-Pro.local>
2018-10-17 15:11:05 +0100

    Construct Request Message

--- HttpInteract.java
+++ HttpInteract.java
@@ -1,104 +1,161 @@
 /*************************************
- * Filename:  HttpInteract.java
- * Names:
- * Student-IDs:
- * Date:
+ * 	Filename:  HttpInteract.java
+ * 	Name: 			Lorenzo Jumilla
+ 		Student-ID:
+ *  Name:				Daniel Stevens
+ 		Student-ID:	201270161
+ * 	Date:				16/10/18
  *************************************/
 
 import java.net.*;
 import java.io.*;
 import java.util.*;
 
-
-/**
- * HttpInteract downloads an object from a given http server
- *
+/*
+ * Class for downloading one object from http server.
  */
- 
- public class HttpInteract {
+
+public class HttpInteract {
 	private String host;
 	private String path;
 	private String requestMessage;
-	private static final String CRLF = "\r\n";
-	
+
+
 	private static final int HTTP_PORT = 80;
-	private static final int BUF_SIZE = 4096;			//Size of chunks (kB)
-	private static final int MAX_OBJECT_SIZE = 102400;	//Max size of object (kB)
-	
-	
-	//Constructor
+	private static final String CRLF = "\r\n";
+	private static final int BUF_SIZE = 4096;
+	private static final int MAX_OBJECT_SIZE = 102400;
+
+ 	/* Create a HttpInteract object. */
 	public HttpInteract(String url) {
+
+		/* Split "URL" into "host name" and "path name", and
+		 * set host and path class variables.
+		 * if the URL is only a host name, use "/" as path
+		 */
+		//Splits URL
+		String[] arrOfUrl = url.split("/", 0);
+	 	//Host Name
+	 	String hostName = (arrOfUrl[0]);
+	 	//Path Name
+		String pathName = (arrOfUrl[1]);
+
+
 		/*
-		Split "URL" and place into host & path
-		if only host name provided, use "/" path
-		*/
-		
-		
+		Construct requestMessage
+		Add a header line */
+		sendCommand("GET" + );
+
+
+		//Carrige return, indicates end of header lines
+		sendCommand(CRLF);
+
+
+
+
+
+
+
+		//Ensure server closes connection after one response.
+		if (response.startsWith("220")) {
+			connection.close();
+		}
+
+
+		/* Fill in */
+
+
+
 		return;
 	}
-	
-	/*send() To Do
-	Send Http request
-	parse response
-	Check for errors
-	return requested object as String if no error
-	*/
+
+
+	/* 	Send Http request
+			parse response and return requested object as a String (if no errors),
+	 		otherwise return meaningful error message.
+	 		Don't catch Exceptions. EmailClient will handle them. */
+
 	public String send() throws IOException {
-		int bodyLength = (-1);
-		int status;
-		String[] tmp;
-		String headers = "";
-		String statusLine = ""; 
-		
-		char[] buf = new char[BUF_SIZE];		//Buffer to read objects in chunks
+
+		/* buffer to read object in 4kB chunks */
+		char[] buf = new char[BUF_SIZE];
+
+		/* Maximum size of object is 100kB, which should be enough for most objects.
+		 * Change constant if you need more. */
 		char[] body = new char[MAX_OBJECT_SIZE];
-		
-		//Open socket & assign read/write streams
-		System.out.println("Connecting server" + host + CRLF);
-		
+
+		String statusLine="";	// status line
+		int status;		// status code
+		String headers="";	// headers
+		int bodyLength=-1;	// lenghth of body
+
+		String[] tmp;
+
+		/* The socket to the server */
 		Socket connection;
-		BufferedReader fromServer;	//Allow fromServer to read from socket
-		DataOutputStream toServer;	//Allow toServer to write to socket
-		
-		System.out.println("Send request:\n" + requestMessage);
-		
-		statusLine = "";
-		System.out.println("Status Line:\n" + statusLine + CRLF);
-		
-		/*
-		Extract status int from statusline
-		If status = 200, close connection & return error msg
-		Convert response message to string
-		Assign headers variable (empty line indicates end of header)
-		Assign Content-Length
-		Assign bodyLength if header line present
+
+		/* Streams for reading from and writing to socket */
+		BufferedReader fromServer;
+		DataOutputStream toServer;
+
+		System.out.println("Connecting server: " + host+CRLF);
+
+		/* Connect to http server on port 80.
+		 * Assign input and output streams to connection. */
+		connection = new Socket("cgi.csc.liv.ac.uk",HTTP_PORT);
+		fromServer = new BufferedReader(isr);
+		toServer = new DataOutputStream(connection.getOutputStream() );
+
+		System.out.println("Send request:\n" + rdequestMessage);
+
+
+		/* Send requestMessage to http server */
+
+
+		/* Read the status line from response message */
+		statusLine= reponse.readLine();
+		System.out.println("Status Line:\n"+statusLine+CRLF);
+
+		/* Extract status code from status line. If status code is not 200,
+		 * close connection and return an error message.
+		 * Do NOT throw an exception */
+		/* Fill in */
+
+		/* Read header lines from response message, convert to a string,
+ 		 * and assign to "headers" variable.
+		 * Recall that an empty line indicates end of headers.
+		 * Extract length  from "Content-Length:" (or "Content-length:")
+		 * header line, if present, and assign to "bodyLength" variable.
 		*/
-		System.out.println("Headers:\n" + headers + CRLF);
-		
-		//Close connection if object size is greater than MAX_OBJECT_SIZE
-		if (true) {
-			return "";
-		};
-		
-		/*
-		Read body in using buf
-		Copy chunks into body[]
-		Stop copying when Content-Length bytes are read or when connection is closed
-		
-		Try to use read(), not readline()
-		*/
+		/* Fill in */ 		// requires about 10 lines of code
+		System.out.println("Headers:\n"+headers+CRLF);
+
+
+		/* If object is larger than MAX_OBJECT_SIZE, close the connection and
+		 * return meaningful message. */
+		if (/* Fill in */) {
+			/* Fill in */
+			return(/* Fill in */ +bodyLength);
+		}
+
+		/* Read the body in chunks of BUF_SIZE using buf[] and copy the chunk
+		 * into body[]. Stop when either we have
+		 * read Content-Length bytes or when the connection is
+		 * closed (when there is no Content-Length in the response).
+		 * Use one of the read() methods of BufferedReader here, NOT readLine().
+		 * Make sure not to read more than MAX_OBJECT_SIZE characters.
+		 */
 		int bytesRead = 0;
-		
-		/*
-		Close connection
-		Return object as string
-		*/
-		
+
+		/* Fill in */   // Requires 10-20 lines of code
+
+		/* At this points body[] should hold to body of the downloaded object and
+		 * bytesRead should hold the number of bytes read from the BufferedReader
+		 */
+
+		/* Close connection and return object as String. */
 		System.out.println("Done reading file. Closing connection.");
 		connection.close();
-		return(	new String(body, 0, bytesRead)	);
-		
+		return(new String(body, 0, bytesRead));
 	}
-	
-	
- }
+}
